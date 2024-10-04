6c41ec7077643ab877f348b76181d9350c24473d
Ubuntu <ubuntu@ip-172-31-91-179.ec2.internal>
2024-10-04 09:34:56 +0000

    initial commit

---
+++ tests/app.test.js
@@ -0,0 +1,9 @@
+const request = require('supertest');
+const app = require('../app');
+
+describe('GET /', () => {
+    it('should respond with Hello, World!', async () => {
+        const response = await request(app).get('/');
+        expect(response.text).toBe('Hello, World!');
+    });
+});
